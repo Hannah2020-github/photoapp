d4ef7f18b6fba1ab627684c5a01e7013018da61c
Hannah2020-github <devonclass712@gmail.com>
2024-08-16 15:52:27 +0800

    feature: 初始 UI 元件

--- app/src/main/java/com/hannah/photoapp/MainActivity.kt
+++ app/src/main/java/com/hannah/photoapp/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.hannah.photoapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,12 +17,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.RecyclerView
 import com.hannah.photoapp.ui.theme.PhotoappTheme
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var searchText: EditText
+    private lateinit var searchBtn: Button
+    private lateinit var progressBar: ProgressBar
+    private lateinit var recyclerview: RecyclerView
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        searchText = findViewById(R.id.search_text)
+        searchBtn = findViewById(R.id.search_btn)
+        progressBar = findViewById(R.id.progressBar)
+        recyclerview = findViewById(R.id.recycler_view)
+        progressBar.visibility = View.INVISIBLE
+
+        searchBtn.setOnClickListener {
+            Toast.makeText(this, "123~~", Toast.LENGTH_SHORT).show()
+        }
+
+
 
     }
 }
